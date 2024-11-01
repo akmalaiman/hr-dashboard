e7a01261ed550f2436deb0553c0b7b9a2249064b
Akmal Aiman Muhammad Ali <akmal.aaiman16@gmail.com>
2024-11-01 15:53:54 +0800

    (#14) - Update the navbar and create sidebar

--- hr-dashboard-ui/src/app/layout/layout.component.ts
+++ hr-dashboard-ui/src/app/layout/layout.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
 import {RouterOutlet} from "@angular/router";
-import {SidebarComponent} from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
         imports: [
                 NavbarComponent,
-                RouterOutlet,
-                SidebarComponent
+                RouterOutlet
         ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
