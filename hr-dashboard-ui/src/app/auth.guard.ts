786e2d74fef9750b9540b93c1a9efe7822fdad48
Akmal Aiman Muhammad Ali <akmal.aaiman16@gmail.com>
2024-11-02 09:42:51 +0800

    (#31) - Automatic logout when jwt expired

--- hr-dashboard-ui/src/app/auth.guard.ts
+++ hr-dashboard-ui/src/app/auth.guard.ts
@@ -1,5 +1,6 @@
-import {CanActivateFn, Router} from '@angular/router';
+import {CanActivateFn, NavigationExtras, Router} from '@angular/router';
 import {inject} from "@angular/core";
+import {jwtDecode} from "jwt-decode";
 
 export const authGuard: CanActivateFn = (route, state) => {
 
@@ -7,6 +8,22 @@ export const authGuard: CanActivateFn = (route, state) => {
         const router = inject(Router);
 
         if (token) {
+                const decodedToken: any = jwtDecode(token);
+                const expirationTime = decodedToken.exp * 1000;
+
+                if (Date.now() > expirationTime) {
+                        console.log('expiration expired');
+                        const navigationExtras: NavigationExtras = {
+                                state: {
+                                        errorMessage: 'You have been logged out due to session expiration. Please log in again.'
+                                }
+                        }
+
+                        localStorage.removeItem('access_token');
+                        router.navigate(['/login'], navigationExtras);
+                        return false;
+                }
+
                 return true;
         } else {
                 router.navigate(['/login']);
