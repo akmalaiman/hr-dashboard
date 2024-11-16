49f4bb5cfa4ffc0b3bed1a612e351c11a090c06d
Akmal Aiman Muhammad Ali <akmal.aaiman16@gmail.com>
2024-11-16 11:35:41 +0800

    (#38) - Create API for add & findByName

--- hr-dashboard-api/src/main/java/aaiman/hrdashboardapi/service/JobPositionService.java
+++ hr-dashboard-api/src/main/java/aaiman/hrdashboardapi/service/JobPositionService.java
@@ -4,6 +4,8 @@ import aaiman.hrdashboardapi.model.JobPosition;
 import aaiman.hrdashboardapi.repository.JobPositionRepository;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,6 +19,23 @@ public class JobPositionService {
 
         public List<JobPosition> getAll() {
                 return jobPositionRepository.findAllByStatus("Active");
+        }
+
+        public JobPosition createJobPosition(JobPosition jobPosition, int userId) {
+
+                JobPosition newJobPosition = new JobPosition();
+                newJobPosition.setName(jobPosition.getName());
+                newJobPosition.setStatus("Active");
+                newJobPosition.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+                newJobPosition.setCreatedBy(userId);
+
+                jobPositionRepository.save(jobPosition);
+                return newJobPosition;
+
+        }
+
+        public JobPosition getActiveJobPositionByName(String name) {
+                return jobPositionRepository.findByNameAndStatus(name, "Active");
         }
 
 }
