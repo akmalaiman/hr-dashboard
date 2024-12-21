da83a78ba38b07b025d3120ad9c4640d92f6ac70
Akmal Aiman Muhammad Ali <akmal.aaiman16@gmail.com>
2024-12-22 00:17:47 +0800

    (#49) - Create base UI for holiday calendar

--- hr-dashboard-ui/src/app/calendar/calendar.component.ts
+++ hr-dashboard-ui/src/app/calendar/calendar.component.ts
@@ -1,12 +1,45 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgIf} from "@angular/common";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
+import {config} from "rxjs";
 
 @Component({
         selector: 'app-calendar',
         standalone: true,
-        imports: [],
+        imports: [
+                RouterLink,
+                NgIf,
+                FormsModule,
+                ReactiveFormsModule
+        ],
         templateUrl: './calendar.component.html',
         styleUrl: './calendar.component.css'
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit{
+
+        pageName: string = 'Calendar';
+        loading: boolean = true;
+        newHolidayForm!: FormGroup;
+        isHolidayExists: boolean = false;
+
+        constructor(config: NgbModalConfig, private modalService: NgbModal, private formBuilder: FormBuilder) {
+                config.backdrop = "static";
+                config.keyboard = false;
+        }
+
+        ngOnInit() {
+                this.newHolidayForm = this.formBuilder.group({
+                        name: ['', [Validators.required]],
+                        holidayDate: ['', [Validators.required]]
+                });
+        }
+
+        openModal(content: any): void {
+                this.modalService.open(content, {centered: true});
+        }
+
+        onSubmit() {}
 
 }
